9dd2834e5eea07cc938997d54a6549dd8201cd96
Soheil <greenroomblackroom@gmail.com>
2025-05-24 20:01:09 +0330

    Added sessions(viewed_posts/keywords)

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,26 @@
 from django.urls import path
 
 from blog.views import set_theme_cookie , get_theme_cookie , delete_theme_cookie
+from blog.views import track_viewed_posts , get_recent_views , empty_viewed_posts , track_keywords , get_recent_keywords , empty_keywords
+
 
 urlpatterns = [
+                       #cookies
+
     path('set_theme_cookie/<str:theme>/', set_theme_cookie, name="set_theme_cookie"),
     path('get_theme_cookie/' , get_theme_cookie , name="get_theme_cookie"),
     path("delete_theme_cookie/", delete_theme_cookie, name="delete_theme_cookie"),
+
+                         #sessions
+
+    path("track_viewed_posts/<int:post_id>/", track_viewed_posts, name="track_viewed_posts"),
+    path("get_recent_views/", get_recent_views, name="get_recent_views"),
+    path("clear_viewed_posts/", empty_viewed_posts, name="empty_viewed_posts"),
+
+
+    path("track_keywords/<str:keyword>/", track_keywords, name="track_keywords"),
+    path("get_recent_keywords/", get_recent_keywords, name="get_recent_keywords"),
+    path("empty_keywords/" , empty_keywords , name="empty_keywords"),
+
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 
+                        #cookies
 
 def set_theme_cookie(request, theme):
     response = HttpResponse(f"Theme set to {theme}")
@@ -22,3 +23,47 @@ def delete_theme_cookie(request):
     response = HttpResponse("Theme is deleted succesfully!")
     response.delete_cookie("theme")  
     return response
+
+
+                         #sessions
+
+def track_viewed_posts(request, post_id):
+    viewed_posts = request.session.get("viewed_posts", [])
+    viewed_posts.insert(0, post_id)
+    viewed_posts = viewed_posts[:5]
+    request.session["viewed_posts"] = viewed_posts
+    request.session.set_expiry(86400)
+    return HttpResponse(f"Viewed posts: {viewed_posts}")
+
+
+
+def get_recent_views(request):
+    viewed_posts = request.session.get("viewed_posts", [])
+    return render(request, "index.html", {"viewed_posts": viewed_posts})
+
+
+
+def empty_viewed_posts(request):
+    del request.session['viewed_posts']
+    return HttpResponse("data deleted!")
+
+
+
+def track_keywords(request, keyword):
+    keywords = request.session.get("keywords", [])
+    keywords.insert(0, keyword)
+    keywords = keywords[:5]
+    request.session["keywords"] = keywords
+    request.session.set_expiry(86400)
+    return HttpResponse(f"Tracked Keywords: {keywords}")
+
+
+
+def get_recent_keywords(request):
+    keywords = request.session.get("keywords", [])
+    return render(request, "index.html", {"keywords": keywords})
+
+
+def empty_keywords(request):
+    del request.session['keywords']
+    return HttpResponse("data deleted!")
