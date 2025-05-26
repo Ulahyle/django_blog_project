0d6a5cc6bda4536ce3ecc25aea4271534204b0e5
Soheil <greenroomblackroom@gmail.com>
2025-05-26 21:43:35 +0330

    solve conflict project.url

--- config/urls.py
+++ config/urls.py
@@ -15,20 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-# <<<<<<< HEAD
 from django.urls import path, include
-from blog.urls import urlpatterns as blog_urls
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('blog/', include(blog_urls)),
-    ]
-# =======
-from django.urls import path , include
-
 from blog.urls import urlpatterns as blog_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/' , include(blog_urlpatterns)),
-# >>>>>>> feature/session-cookies
+    path('blog/', include(blog_urlpatterns)),
 ]
